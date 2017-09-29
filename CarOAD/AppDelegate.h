48670efe86e99363d06a971f9e16deb33c670d14
xfLian <82634907@qq.com>
2017-09-29 10:04:38 +0800

    Initial Commit

---
+++ CarOAD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarOAD
+//
+//  Created by xf_Lian on 2017/9/29.
+//  Copyright © 2017年 xf_Lian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
